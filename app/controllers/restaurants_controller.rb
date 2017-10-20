fedb34468c6568ac5fb08c00f19ecd1c83cac2fe
Nero Siva <neroshan12@gmail.com>
2017-10-20 12:47:03 +0100

    Add a delete feature

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -26,13 +26,20 @@ class RestaurantsController < ApplicationController
   end
 
   def update
-    @restaurant = restaurant.find(params[:id])
+    @restaurant = Restaurant.find(params[:id])
 
     if @restaurant.update(restaurant_params)
       redirect_to @restaurant
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+
+    redirect_to restaurants_path
   end
 
   private
--- spec/features/restaurant_list_spec.rb
+++ spec/features/restaurant_list_spec.rb
@@ -1,16 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe '#Create a new restaurant' do
-    it 'Add Cafe Nero' do
+RSpec.describe '#See a list of restaurants' do
+    it 'Add Cafe Nero and see the restaurant on the restaurants page' do
       visit('restaurants/new')
       fill_in "restaurant[name]", with: 'Cafe Nero'
       fill_in "restaurant[description]", with: 'Awesome place to eat'
       click_button 'Create Restaurant'
+      click_link 'Back'
       expect(page).to have_content 'Cafe Nero'
       expect(page).to have_content 'Awesome place to eat'
   end
 end
-
-
-# page.body
-# capybara open and save method
